489ed381f1b7687d888dc35280a4836427c069c7
Youwen Wu <youwenw@gmail.com>
2025-04-12 23:03:00 -0700

    chore: format code

--- labs/lab02/code/intlist.cpp
+++ labs/lab02/code/intlist.cpp
@@ -1,6 +1,6 @@
 // intlist.cpp
 // Implements class IntList
-// YOUR NAME(S), AND DATE
+// Youwen Wu, AND 4/12/25
 
 #include "intlist.h"
 
@@ -8,80 +8,75 @@
 using std::cout;
 
 // copy constructor
-IntList::IntList(const IntList& source) {
-    //IMPLEMENT THIS
+IntList::IntList(const IntList &source) {
+  // IMPLEMENT THIS
 }
 
 // destructor deletes all nodes
 IntList::~IntList() {
-    //IMPLEMENT THIS
+  // IMPLEMENT THIS
 }
-
 
 // return sum of values in list
 int IntList::sum() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+  return 0; // REPLACE THIS NON-SOLUTION
 }
 
 // returns true if value is in the list; false if not
 bool IntList::contains(int value) const {
-    return false; // REPLACE THIS NON-SOLUTION
+  return false; // REPLACE THIS NON-SOLUTION
 }
 
 // returns maximum value in list, or 0 if empty list
 int IntList::max() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+  return 0; // REPLACE THIS NON-SOLUTION
 }
 
 // returns average (arithmetic mean) of all values, or
 // 0 if list is empty
 double IntList::average() const {
-    return 0.0; // REPLACE THIS NON-SOLUTION
+  return 0.0; // REPLACE THIS NON-SOLUTION
 }
 
 // inserts value as new node at beginning of list
 void IntList::push_front(int value) {
-    // IMPLEMENT
+  // IMPLEMENT
 }
 
 // append value at end of list
 void IntList::push_back(int value) {
-    // IMPLEMENT
- 
+  // IMPLEMENT
 }
 
 // return count of values
 int IntList::count() const {
-   //IMPLEMENT THIS
-   return 0;
+  // IMPLEMENT THIS
+  return 0;
 }
 
-
-//Assignment operator should copy the list from the source
-//to this list, deleting/replacing any existing nodes
-IntList& IntList::operator=(const IntList& source){
-    //IMPLEMENT
-    return *this;
+// Assignment operator should copy the list from the source
+// to this list, deleting/replacing any existing nodes
+IntList &IntList::operator=(const IntList &source) {
+  // IMPLEMENT
+  return *this;
 }
 
 // constructor sets up empty list
-IntList::IntList(){ 
-    //IMPLEMENT THIS 
+IntList::IntList() {
+  // IMPLEMENT THIS
 }
-
 
 // DO NOT CHANGE ANYTHING BELOW (READ IT THOUGH)
 
 // print values enclose in [], separated by spaces
 void IntList::print() const {
-    Node *n = head;
-    cout << '[';
-    while (n) {
-        cout << n->info;
-        if (n->next)
-            cout << " ";
-        n = n->next;
-    }
-    cout << ']';
+  Node *n = head;
+  cout << '[';
+  while (n) {
+    cout << n->info;
+    if (n->next)
+      cout << " ";
+    n = n->next;
+  }
+  cout << ']';
 }
-
